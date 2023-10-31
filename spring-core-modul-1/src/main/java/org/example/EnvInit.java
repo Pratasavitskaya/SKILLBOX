af75284bfdee526d6c1e8b508dc0f7540e164280
ALENAASUS\prata <pratasavitskaya@gmail.com>
2023-10-31 22:19:41 +0300

    работа с файлами

--- spring-core-modul-1/src/main/java/org/example/EnvInit.java
+++ spring-core-modul-1/src/main/java/org/example/EnvInit.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 
 import java.io.*;
 import java.util.HashMap;
@@ -10,15 +11,16 @@ public class EnvInit implements EnvPrinter{
     @Value("${app.env}")
     private String env;
     @Value("${app.file}")
-    private  String pathFile ;
+    Resource resourceFile;
+
     @Override
     public  Map<String,Contacts> printEnv() {
+
         System.out.println("init work");
             Contacts contact;
             Map<String,Contacts> mapContacts = new HashMap<>();
             try {
-
-                File file = new File(pathFile);
+               File file = resourceFile.getFile();
 
                 FileReader fr = new FileReader(file);
 
@@ -43,5 +45,6 @@ public class EnvInit implements EnvPrinter{
             }
             return mapContacts;
         }
+
 
 }
--- spring-core-modul-1/src/main/java/org/example/WorkWithFile.java
+++ spring-core-modul-1/src/main/java/org/example/WorkWithFile.java
@@ -1,22 +1,25 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class WorkWithFile {
 
-    @Value("${app.file}")
-    private  String pathFile ;
+    @Value("${app.fileToSave}")
+    private  String pathFileToSave ;
 
 
     public  void writeToFile(Map<String,Contacts> mapContacts) throws IOException {
-        
-        try(FileWriter writer = new FileWriter(pathFile, false))
+
+        try(FileWriter writer = new FileWriter(pathFileToSave, false))
         {
             for(Map.Entry<String, Contacts> entry : mapContacts.entrySet()){
 
@@ -32,36 +35,5 @@ public class WorkWithFile {
             System.out.println(ex.getMessage());
         }
 
-
     }
-//    public Map<String,Contacts> readFile(){
-//
-//        Contacts contact;
-//        Map<String,Contacts> mapContacts = new HashMap<>();
-//        try {
-//
-//            File file = new File(pathFile);
-//
-//            FileReader fr = new FileReader(file);
-//
-//            BufferedReader reader = new BufferedReader(fr);
-//
-//            String line = reader.readLine();
-//            String[] info = line.split(";");
-//            contact=new Contacts(info[0],info[1],info[2]);
-//            mapContacts.put(info[2],contact);
-//            while (line != null) {
-//                info = line.split(";");
-//                contact=new Contacts(info[0],info[1],info[2]);
-//                mapContacts.put(info[2],contact);
-//                line = reader.readLine();
-//            }
-//
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return mapContacts;
-//    }
 }
